ba409466014c4c78be11952b2e2734184e247fc5
Nasser <abdo.abdo3003@gmail.com>
2024-01-02 17:20:18 -0500

    jan 2nd 24

--- src/main/java/com/dailyAssignments/Main.java
+++ src/main/java/com/dailyAssignments/Main.java
@@ -1,19 +1,41 @@
 package com.dailyAssignments;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        PlaylisManager playlisManager = new PlaylisManager();
-        playlisManager.addSong("ABC");
-        playlisManager.addSong("123");
-        playlisManager.addSong("asdada");
-        playlisManager.addSong("dadadadada");
+        System.out.println(reverse("Hello World!"));
+        System.out.println(isBalanced("((((((()))))"));
+        System.out.println(isBalanced("(((())))"));
+    }
+    public static String reverse(String str) {
+        StringBuilder output = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
 
-        System.out.println("-----------------------------------");
-        playlisManager.displayPlaylist();
-        System.out.println("-----------------------------------");
-        playlisManager.play();
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+        }
+        while (!stack.isEmpty()) {
+            output.append(stack.pop());
+        }
+        return String.valueOf(output);
+    }
+    public static boolean isBalanced(String str) {
+        // assuming () doesn't have to be back to back.
+        List<Character> openParanthesisList = new LinkedList<>();
+        List<Character> closedParanthesisList = new LinkedList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                openParanthesisList.add(str.charAt(i));
+                continue;
+            }
+            if (str.charAt(i) == ')') {
+                closedParanthesisList.add(str.charAt(i));
+            }
+        }
+        return openParanthesisList.size() == closedParanthesisList.size();
     }
 }
