800d344c7888042b9858bae03f320f69102fd8d3
Nasser <abdo.abdo3003@gmail.com>
2024-01-11 09:13:17 -0500

    jan 10th

--- src/main/java/com/dailyAssignments/Main.java
+++ src/main/java/com/dailyAssignments/Main.java
@@ -1,9 +1,25 @@
 package com.dailyAssignments;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
+        Map<Character, Integer> charOccurrencesTracker = new HashMap<>();
+        String sentence = "The quick brown fox jumps over the lazy dog.";
+//        String sentence = "Testing 1 2 3 4 ... testing 1 2 3 4";
 
+        String englishCharactersRegex = "[a-zA-Z]+";
+        Pattern pattern = Pattern.compile(englishCharactersRegex);
 
+        for (int i = 0; i < sentence.length(); i++) {
+            char currentChar = sentence.charAt(i);
+            Matcher matcher = pattern.matcher(String.valueOf(currentChar));
+            if (matcher.matches())
+                charOccurrencesTracker.put(currentChar, charOccurrencesTracker.getOrDefault(currentChar, 0) + 1);
+        }
+        System.out.println(charOccurrencesTracker);
     }
 }
