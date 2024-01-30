28672dfd903d065216612636d492fb876fb0ba97
Nasser <abdo.abdo3003@gmail.com>
2024-01-29 23:42:23 -0500

    why is it not printing each word in a new line?

--- src/main/java/com/dailyAssignments/Main.java
+++ src/main/java/com/dailyAssignments/Main.java
@@ -1,9 +1,26 @@
 package com.dailyAssignments;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Wednesday.spellName("Nasser");
-        System.out.println("----------------");
-        Wednesday.spellNameList("Nasser");
+        String pathToRead = "src/main/java/com/dailyAssignments/data/words_alpha.txt";
+        String pathToWrite = "src/main/java/com/dailyAssignments/data/long_words.txt";
+
+        Reader reader = new Reader();
+        Writer writer = new Writer();
+
+        System.out.println(reader.findLongestWordAmazing(pathToRead));
+        System.out.println(reader.findLongestWordRegular(pathToRead));
+
+        List<String> OverNumberLetters = reader.findWordsOverNLetters(pathToRead, 24);
+        writer.writeIntoFile(pathToWrite, OverNumberLetters);
+
+        reader.printAllWords(pathToWrite);
+        reader.printAllWordsTheLazyWay(pathToWrite);
+        reader.printAllWordsTheCoolWay(pathToWrite);
+
+
+
     }
 }
